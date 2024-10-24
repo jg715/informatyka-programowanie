98d6eb887020c809b19d9231e150c3241842200e
jg715 <jgajowiecki714@gmail.com>
2024-10-24 09:17:56 +0200

    Komentarz

---
+++ lekcja pierwsza/1324.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int liczba1 = 5;
+	int liczba2 = 3;
+	
+	cout << "suma:"    << liczba1 + liczba2 << endl;
+	cout << "roznica:" << liczba1 - liczba2 << endl;
+	cout << "iloczyn:" <<liczba1 * liczba2;
+	
+	return 0;
+}
---
+++ lekcja pierwsza/c++ 123.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	cout << "Witaj w programowaniu w c++";
+	
+	return 0;
+}
